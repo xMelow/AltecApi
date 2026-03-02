b07db64b4fc12ca44b7c455d630374fb9957b81c
Flor Stellamans <florstellamamans@gmail.com>
2026-03-02 11:00:14 +0100

    initial commit

---
+++ Altec.Api/Controllers/TsplController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Altec.Api.Controllers;
+
+[ApiController]
+[Route("tspl")]
+public class TsplController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Ok! This is the tpsl endpoint");
+    }
+}
+
+public record TsplRequest(string Code);
