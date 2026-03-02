907e5bae3ae7daf7c15fc32276e82bfffab511a5
Flor Stellamans <florstellamamans@gmail.com>
2026-03-02 13:45:14 +0100

    implement TsplParser class

--- Altec.Api/Domain/Tspl/TsplParser.cs
+++ Altec.Api/Domain/Tspl/TsplParser.cs
@@ -1,6 +1,35 @@
-﻿namespace Altec.Api.Domain.Tspl;
+﻿using Altec.Api.Records;
+
+namespace Altec.Api.Domain.Tspl;
 
 public class TsplParser
 {
+    private IReadOnlyList<string> commandsToParse = new[]
+    {
+        "SIZE", "BAR", "BOX", "TEXT", "BARCODE", "QRCODE", "CIRCLE", "PUTBMP"
+    };
     
+    public IReadOnlyList<TsplCommand> Parse(string tspl)
+    {
+        var result = new List<TsplCommand>();
+        string[] lines = tspl.Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+        
+        foreach (var line in lines)
+        {
+            var name = line.Split(" ")[0];
+            if (commandsToParse.Contains(name))
+            {
+                result.Add(ParseTsplLine(line));
+            }
+        }
+        return result;
+    }
+
+    private TsplCommand ParseTsplLine(string line)
+    {
+        var name = line.Split(" ")[0];
+        string[] data = line.Split(" ")[1].Split(",");
+
+        return new TsplCommand(name, data);
+    }
 }
